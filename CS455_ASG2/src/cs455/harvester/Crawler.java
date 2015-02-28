4f436f3c648447f7710feb18f4fdf78fba23904a
Shaun Parkison <shacklefurd@gmail.com>
2015-02-28 16:19:17 -0700

    Updated Crawler constructor

--- CS455_ASG2/src/cs455/harvester/Crawler.java
+++ CS455_ASG2/src/cs455/harvester/Crawler.java
@@ -9,14 +9,29 @@ package cs455.harvester;
 public class Crawler {
 
 	// Instance variables **************
-	// TO-DO
+	private int port;
+	private int poolSize;
+	private String rootUrl;
+	private String configPath;
 	
 	public static void main(String[] args) throws InterruptedException {
-
+		if(args.length < 3){
+			System.out.println("Incorrect format used to initate Crawler\nPlease use: cs455.harvester.Crawler [portNum] [poolSize] [rootUrl] [configPath]");
+		}else{
+			int port = Integer.parseInt(args[0]);
+			int poolSize = Integer.parseInt(args[1]);
+			String rootUrl = args[2];
+			String configPath = args[3];
+			new Crawler(port, poolSize, rootUrl, configPath);
+		}		
 	}
-	
+
 	public Crawler(int port, int poolSize, String rootUrl, String configPath){
-		
+		this.port = port;
+		this.poolSize = poolSize;
+		this.rootUrl = rootUrl;
+		this.configPath = configPath;
+		// Do some stuff!!
 	}
 
 
