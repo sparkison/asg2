d19c465bf088dfd890f3ab81fb495bd2ef94f1a5
Shaun Parkison <shacklefurd@gmail.com>
2015-03-09 11:37:11 -0600

    Updated Adjacency list with try/finally to ensure crawlers shut down.

--- CS455_ASG2/src/cs455/util/AdjacencyList.java
+++ CS455_ASG2/src/cs455/util/AdjacencyList.java
@@ -29,6 +29,7 @@ public class AdjacencyList {
 	private final String ROOT_URL;
 	private final Crawler CRAWLER;
 	private final File FILE;
+	private final boolean debug = true;
 
 	public AdjacencyList(String rootUrl, Crawler crawler){
 		ADJACENCY = new HashMap<String, Set<String>>();
@@ -51,82 +52,91 @@ public class AdjacencyList {
 	 * Initiates the creation of the directory structure for this Crawler
 	 */
 	public void startDirectoryCreation(){
-		System.out.println("Creating directory for Crawler: " + ROOT_URL + "...");
+		if(debug)
+			System.out.println("Creating directory for Crawler: " + ROOT_URL + "...");
 
-		String nodePath = FILE.getAbsolutePath();
-		for(String vertex : ADJACENCY.keySet()){
-			if(vertex.contains(ROOT_URL)){
+		try{
+			String nodePath = FILE.getAbsolutePath();
+			for(String vertex : ADJACENCY.keySet()){
+				if(vertex.contains(ROOT_URL)){
+					try {
+
+						/*
+						 * Build upper level folder for the vertex
+						 */
+						String nodeFolder = getDirectoryName(vertex);
+						if(nodeFolder.equals(""))
+							nodeFolder = ROOT_URL.replaceAll("[^a-zA-Z0-9._-]", "-");
+						else if(nodeFolder.charAt(0) == '-')
+							nodeFolder = nodeFolder.substring(1);
+
+						File file = new File(nodePath + "/" + nodeFolder);
+						if (!file.exists()) {
+							file.mkdirs();
+						}
+
+						/*
+						 * Build the out file
+						 */
+						File outFile = new File(file.getAbsolutePath() + "/out");
+						List<String> out = outEdges(vertex);
+						try {
+							PrintWriter outWriter = new PrintWriter(outFile);
+							for(String edge : out){
+								outWriter.println(edge);
+							}
+							outWriter.flush();
+							outWriter.close();
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						}
+
+						/*
+						 * Build the in file
+						 */
+						File inFile = new File(file.getAbsolutePath() + "/in");
+						List<String> in = inEdges(vertex);
+						try {
+							PrintWriter inWriter = new PrintWriter(inFile);
+							for(String edge : in){
+								inWriter.println(edge);
+							}
+							inWriter.flush();
+							inWriter.close();
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						}
+
+					} catch (MalformedURLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				/*
+				 * Write the broken links file
+				 */
+				File brokenLinks = new File(NODE_ROOT + "/broken-links");
 				try {
-
-					/*
-					 * Build upper level folder for the vertex
-					 */
-					String nodeFolder = getDirectoryName(vertex);
-					if(nodeFolder.equals(""))
-						nodeFolder = ROOT_URL.replaceAll("[^a-zA-Z0-9._-]", "-");
-					else if(nodeFolder.charAt(0) == '-')
-						nodeFolder = nodeFolder.substring(1);
-
-					File file = new File(nodePath + "/" + nodeFolder);
-					if (!file.exists()) {
-						file.mkdirs();
+					PrintWriter brokenLink = new PrintWriter(brokenLinks);
+					for(String broken : BROKEN_LINKS){
+						brokenLink.println(broken);
 					}
-
-					/*
-					 * Build the out files
-					 */
-					File outFile = new File(file.getAbsolutePath() + "/out");
-					List<String> out = outEdges(vertex);
-					try {
-						PrintWriter outWriter = new PrintWriter(outFile);
-						for(String edge : out){
-							outWriter.println(edge);
-						}
-						outWriter.flush();
-						outWriter.close();
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-					}
-
-					/*
-					 * Build the in files
-					 */
-					File inFile = new File(file.getAbsolutePath() + "/in");
-					List<String> in = inEdges(vertex);
-					try {
-						PrintWriter inWriter = new PrintWriter(inFile);
-						for(String edge : in){
-							inWriter.println(edge);
-						}
-						inWriter.flush();
-						inWriter.close();
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-					}
-
-				} catch (MalformedURLException e) {
+					brokenLink.flush();
+					brokenLink.close();
+				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				}
+				
 			}
+		}finally{
+			
+			if(debug)
+				System.out.println("Finished creating directories.");
+
+			// All done, shut it down...
+			CRAWLER.stop();
 		}
 
-		/*
-		 * Write the broken links file
-		 */
-		File brokenLinks = new File(NODE_ROOT + "/broken-links");
-		try {
-			PrintWriter brokenLink = new PrintWriter(brokenLinks);
-			for(String broken : BROKEN_LINKS){
-				brokenLink.println(broken);
-			}
-			brokenLink.flush();
-			brokenLink.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		// All done, shut it down...
-		CRAWLER.stop();
 	}
 
 	/**
